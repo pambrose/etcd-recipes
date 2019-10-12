95437e6a1e6790e0a09798698fe8b801dffb8f75
Paul Ambrose <pambrose@mac.com>
2019-10-12 10:48:47 -0700

    Add DistributedBarrierWithCountTest

--- src/test/kotlin/io/etcd/recipes/barrier/DistributedBarrierWithCountTests.kt
+++ src/test/kotlin/io/etcd/recipes/barrier/DistributedBarrierWithCountTests.kt
@@ -20,9 +20,11 @@ package io.etcd.recipes.barrier
 
 import com.sudothought.common.util.random
 import com.sudothought.common.util.sleep
+import io.etcd.recipes.common.nonblockingThreads
+import org.amshove.kluent.shouldEqual
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CountDownLatch
-import kotlin.concurrent.thread
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.time.seconds
 
 class DistributedBarrierWithCountTests {
@@ -32,36 +34,41 @@ class DistributedBarrierWithCountTests {
         val urls = listOf("http://localhost:2379")
         val path = "/barriers/${javaClass.simpleName}"
         val count = 30
-        val waitLatch = CountDownLatch(count)
+        val retryAttempts = 5
         val retryLatch = CountDownLatch(count - 1)
+        val retryCounter = AtomicInteger(0)
+        val advancedCounter = AtomicInteger(0)
 
         DistributedBarrierWithCount.delete(urls, path)
 
         fun waiter(id: Int, barrier: DistributedBarrierWithCount, retryCount: Int = 0) {
-            sleep(10.random.seconds)
+
+            sleep(5.random.seconds)
             println("#$id Waiting on barrier")
 
             repeat(retryCount) {
-                barrier.waitOnBarrier(2.seconds)
+                barrier.waitOnBarrier(1.seconds)
                 println("#$id Timed out waiting on barrier, waiting again")
+                retryCounter.incrementAndGet()
             }
 
             retryLatch.countDown()
+
             println("#$id Waiter count = ${barrier.waiterCount}")
             barrier.waitOnBarrier()
 
+            advancedCounter.incrementAndGet()
+
             println("#$id Done waiting on barrier")
-            waitLatch.countDown()
         }
 
-        repeat(count - 1) { i ->
-            thread {
+        val (finishedLatch, holder) =
+            nonblockingThreads(count - 1) { i ->
                 DistributedBarrierWithCount(urls, path, count)
                     .use { barrier ->
-                        waiter(i, barrier, 5)
+                        waiter(i, barrier, retryAttempts)
                     }
             }
-        }
 
         retryLatch.await()
         sleep(2.seconds)
@@ -71,7 +78,10 @@ class DistributedBarrierWithCountTests {
                 waiter(99, barrier)
             }
 
-        waitLatch.await()
+        finishedLatch.await()
+
+        retryCounter.get() shouldEqual retryAttempts * (count - 1)
+        advancedCounter.get() shouldEqual count
 
         println("Done")
     }
